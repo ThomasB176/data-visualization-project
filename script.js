4b03b82ca3a16bb02963d8aeb234bae1789b23e8
ThomasB176 <Thomasb176@nycstudents.net>
2025-12-01 12:48:22 -0500

    Replace fetch with fs.readFile for CSV data

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-fetch('data/MUSEUM.csv')
-.then(response => response.text)
-.then(data => {
+const fs = require('fs');
+
+fs.readFile('data/MUSEUM.csv', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
     console.log(data);
 });
