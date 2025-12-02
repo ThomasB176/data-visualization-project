8c5b65aa5ae354b8403a1bb7c7333f22d996c09b
ThomasB176 <Thomasb176@nycstudents.net>
2025-12-02 00:57:42 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const fs = require('fs');
+fetch("data/MUSEUM.csv")
+    .then(response => response.text())
+    .then(csv => {
+        const rows = csv.split("\n").slice(1);
+        const labels = [];
+        const values = [];
 
-fs.readFile('data/MUSEUM.csv', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+        rows.forEach(row => {
+            const cols = row.split(",");
+            labels.push(cols[0]);
+            values.push(parseInt(cols[1]));
+        });
+        
+const ctx = document.getElementById( 'myChart');
+
+    new Chart(ctx, { 
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Example Data',
+                data: values,
+                borderWidth: 1,
+                backgroundColor: ['#ff6384', '#36a2eb', '#ffcd56', '#4bc0cO', '#000000'],
+            }]
+        },
+        options: {
+            scales: {   
+                y: {beginAtZero: true }
+            }
+        }
+    });
 });
